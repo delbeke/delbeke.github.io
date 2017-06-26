b0c1ff732409ad9d55f55c805fa3ce92ec1b1bb2
Pieter Delbeke <pieter.delbeke@fabtiek.be>
2017-06-26 14:21:33 +0200

    first commit

---
+++ application.js
@@ -0,0 +1,23 @@
+App.onLaunch =  function(options) {
+
+}
+
+App.onWillResignActive = function () {
+
+}
+
+App.onDidEnterBackground = function () {
+
+}
+
+App.onWillEnterForeground = function () {
+
+}
+
+App.onDidBecomeActive = function () {
+
+}
+
+App.onWillTerminate = function () {
+
+}
